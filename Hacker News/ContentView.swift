378b4d28adea7800cb455fbd61ea0f45deec1876
Demario Douce <demario.douce567@gmail.com>
2020-06-02 12:06:38 -0400

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Demario Douce on 2020-06-02.
+//  Copyright © 2020 Demario Douce. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
