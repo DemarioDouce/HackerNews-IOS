a4da8fe29e22f70d1e83f8cb4136d49e7263fc68
Demario Douce <demario.douce567@gmail.com>
2020-06-02 14:09:23 -0400

    Networking logic added.

---
+++ Hacker News/Model/PostData.swift
@@ -0,0 +1,26 @@
+//
+//  PostData.swift
+//  Hacker News
+//
+//  Created by Demario Douce on 2020-06-02.
+//  Copyright © 2020 Demario Douce. All rights reserved.
+//
+
+import UIKit
+
+struct Results:Decodable {
+    
+    let hits: [Post]
+    
+}
+
+struct Post:Decodable, Identifiable {
+    var id: String {
+        
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
